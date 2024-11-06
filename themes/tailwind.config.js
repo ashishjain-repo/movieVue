55f10f657c0d6ee21ef6bdc15a6d32e569a59bcb
Ashish Jain <ashishjain99@yahoo.com>
2024-11-06 15:55:46 -0700

    Finished Navbar working on Main

--- themes/tailwind.config.js
+++ themes/tailwind.config.js
@@ -2,6 +2,13 @@
 module.exports = {
   content: ["./**/*.{html,js}"],
   theme: {
+    screens:{
+      xs:'480px',
+      sm:'640px',
+      md:'768px',
+      lg:'1024px',
+      xl:'1280px',
+    },
     extend: {},
   },
   plugins: [],
