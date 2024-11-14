fe3daa71035de7b79ab1caa055d4c12f8c056fff
Ashish Jain <ashishjain99@yahoo.com>
2024-11-13 20:15:00 -0700

    Added IMDB Top 5 movies

--- movieDB/src/components/NavbarTop.vue
+++ movieDB/src/components/NavbarTop.vue
@@ -2,5 +2,18 @@
 </script>
 
 <template>
-    
+    <nav class="grid grid-cols-2 xs:grid-cols-3 lg:grid-cols-4 xl:grid-cols-5 justify-center items-center align-middle text-center py-4 bg-black text-white shadow-sm shadow-slate-500">
+        <div class="">
+            <a href="" class="text-3xl lg:text-4xl kanit-black">MovieDB</a>
+        </div>
+        <div class="hidden xs:flex"></div>
+        <div class="hidden lg:flex"></div>
+        <div class="hidden xl:flex"></div>
+        <div class="flex justify-evenly gap-2 sm:gap-1 font-semibold lg:text-xl align-middle items-center">
+            <a href="" class="hover:text-amber-400 transition-all duration-200">Home</a>
+            <a href="" class="hover:text-amber-400 transition-all duration-200">Movies</a>
+            <a href="" class="hover:text-amber-400 transition-all duration-200">TV</a>
+        </div>
+    </nav>
+    <!-- FOr Router  border-2 border-gray-300 p-1 rounded-md bg-gray-700 -->
 </template>
--- movieDB/src/views/HomeView.vue
+++ movieDB/src/views/HomeView.vue
@@ -1,8 +1,21 @@
 <script setup>
 import NavbarTop from '@/components/NavbarTop.vue';
+import NavbarBottom from '@/components/NavbarBottom.vue';
+import TopMovies from '@/components/TopMovies.vue';
 
 </script>
 
 <template>
-<NavbarTop />
+    <body class="bg-gradient-to-r from-slate-900 to-slate-700">
+        <NavbarTop />
+        <main>
+            <section class="text-center py-5">
+            <h1 class="text-5xl xs:text-6xl sm:text-7xl lg:text-8xl text-white font-bold my-5 kanit-black">MovieDB</h1>
+            <p class="text-2xl xs:text-3xl sm:text-4xl lg:text-5xl text-white font-bold">Your Movie Guide</p>
+            <hr class="m-10">
+            </section>
+            <TopMovies />
+        </main>
+        <NavbarBottom />
+    </body>
 </template>
